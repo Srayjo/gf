dafdeac9726f568fc4a6057ca54ab915745c7f81
성명규(20181177) <90017907+Srayjo@users.noreply.github.com>
2021-09-09 12:03:06 +0000

    게임 루프 실습

--- main.cpp
+++ main.cpp
@@ -2,30 +2,25 @@
 #include<SDL.h>
 SDL_Window* g_pWindow=0;
 SDL_Renderer* g_pRenderer=0;
+bool g_bRunning = false;
 
 int main(int argc, char* args[])
 {
-  if(SDL_Init(SDL_INIT_EVERYTHING)>=0)
+    if(init("Breaking Up HelloSDL",SDL_WINDOWPOS_CENTERED,       SDL_WINDOWPOS_CENTERED,640,480, SDL_WINDOW_SHOWN))
   {
-    g_pWindow=SDL_CreateWindow("Setting up SDL",SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED,640,480,SDL_WINDOW_SHOWN);
-
-    if(g_pWindow !=0)
-    {
-      g_pRenderer=SDL_CreateRenderer(g_pWindow,-1,0);
-    }
+    g_bRunning=true;
   }
   else
   {
     return 1;
   }
 
-  SDL_SetRenderDrawColor(g_pRenderer,0,0,0,255);
-  SDL_RenderClear(g_pRenderer);
-  SDL_RenderPresent(g_pRenderer);
+  while(g_bRunning)
+  {
+    render();
+  }
 
-  SDL_Delay(5000);
   SDL_Quit();
-
   return 0;
 }
 // 명규 ... 
