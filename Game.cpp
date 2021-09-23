6941a91f9757080bc39bfa160a0ad1441b26643a
성명규(20181177) <90017907+Srayjo@users.noreply.github.com>
2021-09-23 13:08:13 +0000

    SDL_IMAGE

--- Game.cpp
+++ Game.cpp
@@ -1,17 +1,18 @@
 #include "Game.h"
 #include "SDL.h"
+#include "SDL_image.h"
 
 bool Game::Init(const char *title, int xpos, int ypos, int width, int height, int flags) // 1
 {
   // 윈도우 * 랜더러 등 생성을 위해 SDL_Init() 정의
   if (SDL_Init(SDL_INIT_EVERYTHING)==0){
-    m_pWindow = SDL_CreateWindow(title,xpos,ypos,width,height,SDL_WINDOW_MAXIMIZED);
+    m_pWindow = SDL_CreateWindow(title,xpos,ypos,width,height,SDL_WINDOW_SHOWN);
     
     if (m_pWindow !=0){
       m_pRenderer = SDL_CreateRenderer(m_pWindow,-1,0);
 
       if(m_pRenderer !=0){
-        SDL_SetRenderDrawColor(m_pRenderer,255,255,255,255);
+        SDL_SetRenderDrawColor(m_pRenderer,255,0,0,255);
       } else{
         return false; // 랜더링 생성 실패
       }
@@ -19,20 +20,25 @@ bool Game::Init(const char *title, int xpos, int ypos, int width, int height, in
         return false; // 윈도우가 거짓이기에 생성 실패 
       } 
 
-      SDL_Surface* pTempSurface = SDL_LoadBMP("Assets/rider.bmp");
+      // SDL_Surface* pTempSurface = SDL_LoadBMP("Assets/rider.bmp");
+
+      // png 파일을 불러오기 위한 image.h의 함수 호출
+      SDL_Surface* pTempSurface=IMG_Load("Assets/animate-alpha.png");
 
       m_pTexture = SDL_CreateTextureFromSurface(m_pRenderer,pTempSurface);
       // SurFace에 저장된 rider.bmp 를 받고 화면에 그려줌
 
       SDL_FreeSurface(pTempSurface);
 
-      SDL_QueryTexture(m_pTexture,NULL,NULL,&m_sourceRectangle.w,
-      &m_sourceRectangle.h); // 원본 상자의 너비*높이 설정
+     /* SDL_QueryTexture(m_pTexture,NULL,NULL,&m_sourceRectangle.w,
+      &m_sourceRectangle.h); // 원본 상자의 너비*높이 설정 */
 
+      m_sourceRectangle.w=128;
+      m_sourceRectangle.h=82;
 
       // 대상상자 너비*높이 설정
-      m_destinationRectangle.w=600; 
-      m_destinationRectangle.h=350;
+      m_destinationRectangle.w=m_sourceRectangle.w; 
+      m_destinationRectangle.h=m_sourceRectangle.h;
 
 
       // 원본&대상상자의 위치 설정
@@ -48,6 +54,7 @@ bool Game::Init(const char *title, int xpos, int ypos, int width, int height, in
 
   void Game::update()
   {
+    m_sourceRectangle.x=128*((SDL_GetTicks()/100)%6);
   }
 
   void Game::render()
