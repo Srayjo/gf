135c26695d2fb6a82a66123415f163dbf9fc0157
성명규(20181177) <90017907+Srayjo@users.noreply.github.com>
2021-10-27 13:48:37 +0000

    스프라이트 실습 1차

--- Game.cpp
+++ Game.cpp
@@ -12,21 +12,20 @@ bool Game::init(const char* title, int xpos, int ypos, int width, int height, in
       } else{return false;}
     } else{return false;}
   } else{return false;}
-  SDL_Surface* pTempSurface =SDL_LoadBMP("Assets/rider.bmp");
+
+  SDL_Surface* pTempSurface =SDL_LoadBMP("Assets/animate.bmp");
   m_pTexture = SDL_CreateTextureFromSurface(m_pRenderer,pTempSurface);
 
   SDL_FreeSurface(pTempSurface);
 
-  SDL_QueryTexture(m_pTexture,NULL,NULL,&m_sourceRectangle.w,&m_sourceRectangle.h);
-  
-  m_sourceRectangle.x=0;
-  m_sourceRectangle.y=0;
+  m_sourceRectangle.w=128;
+  m_sourceRectangle.h=82;
 
   m_destinationRectangle.w = m_sourceRectangle.w;
   m_destinationRectangle.h = m_sourceRectangle.h;
 
-  m_destinationRectangle.x = 0;
-  m_destinationRectangle.y= 0;
+  m_destinationRectangle.x = m_sourceRectangle.x=0;
+  m_destinationRectangle.y= m_sourceRectangle.y =0;
   
   m_bRunning = true;
 return true;
@@ -34,7 +33,7 @@ return true;
 
 void Game::update()
 {
-
+  m_sourceRectangle.x =128*((SDL_GetTicks()/100)%6);
 }
 void Game::render()
 {
